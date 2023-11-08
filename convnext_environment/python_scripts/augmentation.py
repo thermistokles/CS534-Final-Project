1cfb4753251b166ce226564bf5795f6a2ff30108
michael-mcinerney <mamcinerney@wpi.edu>
2023-11-08 17:29:05 -0500

    resize applied post-transform

--- convnext_environment/python_scripts/augmentation.py
+++ convnext_environment/python_scripts/augmentation.py
@@ -5,6 +5,7 @@ import albumentations as A
 import cv2
 import random
 
+print("Reading images from original training set serialization")
 imageData=pd.read_csv('stage_1_train_images.csv')
 
 images='png_images'
@@ -16,12 +17,13 @@ imageData['mpath']=imageData['new_filename'].apply(lambda x:os.path.join(masks,x
 
 il_pos = []
 il_neg = []
+print("Preprocessing images from original training set")
 for i in range(len(imageData)):
     image = imageData.path.iloc[i]
     if(imageData.label.iloc[i]==1):
-        il_pos.append(cv2.resize(cv2.imread(image,cv2.IMREAD_COLOR),[224,224]))
+        il_pos.append(cv2.imread(image,cv2.IMREAD_COLOR))
     else:
-        il_neg.append(cv2.resize(cv2.imread(image,cv2.IMREAD_COLOR),[224,224]))
+        il_neg.append(cv2.imread(image,cv2.IMREAD_COLOR))
 
 npILpos = np.array(il_pos)
 npILneg = np.array(il_neg)
@@ -31,13 +33,18 @@ transform = A.Compose([
     A.ShiftScaleRotate(shift_limit=0, scale_limit=0.15, rotate_limit=0, p=1),
 ])
 
+print("Running image transforms")
 for i in range(100):
+    if i%5==0:
+        print(f"Iteration {i} in progress...   ",end=" ")
     pos_img = random.choice(il_pos)
     neg_img = random.choice(il_neg)
-    pos_img_transformed = transform(image=pos_img)["image"]
-    neg_img_transformed = transform(image=neg_img)["image"]
-    pos_filename = f"posadjusted-{i:05d}.png"
-    neg_filename = f"negadjusted-{i:05d}.png"
+    pos_img_transformed = cv2.resize(transform(image=pos_img)["image"],[224,224])
+    neg_img_transformed = cv2.resize(transform(image=neg_img)["image"],[224,224])
+    pos_filename = f"augmentation/posadjusted-{i:05d}.png"
+    neg_filename = f"augmentation/negadjusted-{i:05d}.png"
     cv2.imwrite(pos_filename,pos_img_transformed)
     cv2.imwrite(neg_filename,neg_img_transformed)
+print("")
+print("Image transformation completed")
     
